9f31a235789ea30565fe7d4e0a0582f58d18b116
BalameenaN <balameena.navukkarasu@gmail.com>
2025-03-07 11:06:06 -0700

    Login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
      <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Login" element={<Login />}/>
      </Routes>
     </>
   )
