f3ec673bacc90796ae587771124c6c29ebbc7e63
Fouad-Mesrane <fouadmesrane@gmail.com>
2025-03-07 14:07:59 -0700

    fixed components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { PostForm } from './PostForm'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+     
     </div>
   )
 }
