a08244c85d17d607fa3150bca6ea096f86b351e2
Fouad-Mesrane <fouadmesrane@gmail.com>
2025-03-07 10:07:24 -0700

    added MVC Model

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.use(cors());
 
 
 
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port ", process.env.PORT);
     connectDB();
