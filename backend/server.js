e4beea330374709ef6cd40696e08661441391480
Fouad-Mesrane <fouadmesrane@gmail.com>
2025-03-07 10:00:51 -0700

    structure

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port ", process.env.PORT);
+    connectDB();
+});
