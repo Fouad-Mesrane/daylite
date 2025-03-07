50c3e1269a96b746693309544eb2adcfef9372c6
Fouad-Mesrane <fouadmesrane@gmail.com>
2025-03-07 14:01:10 -0700

    fixed post  routes

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -1,27 +1,10 @@
 import express from "express";
-import { Post } from "../models/PostsModel.js";
+import { createPost, deletePost } from "../controllers/postsControllers.js";
 
 const router = express.Router();
 
-router.post('/posts', async (req, res) =>{
-    try{
-        const newPost = new Post({
-            content: req.body.content
-        })
-        const savedPost = await newPost.save();
-        res.status(200).json(savedPost)
-    } catch(err) {
-        res.status(400).json({err: 'Error creating post', details: err.message})
-    }
-});
+router.post("/", createPost);
 
-router.delete('/posts/:id', async (req, res) => {
-    try {
-        const deletedPost = await Post.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: "Post deleted successfully"})
-    } catch(err) {
-        res.status(400).json({ err: 'Error deleting post', details: err.message})
-    }
-});
+router.delete("/:id", deletePost);
 
-export default router
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
-import { Post } from "./models/PostsModel.js";
+
 
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", postsRoutes)
+app.use("/api/posts", postsRoutes)
 
 app.get("/", (req, res) => {
     res.send("API Running");
